2c6da9ab39a6b158866bd22bac8e9f7c137a49ed
mattchristenson <matt@rcc.team>
2023-05-08 15:00:31 -0500

    checkpoint

---
+++ lecturedemos/2-11.js
@@ -0,0 +1,20 @@
+let kids = 
+[
+    "Natalie", 
+    "Brittany", 
+    "Zachary",
+    "Matthew",
+    "Sarah"
+];
+
+
+console.log(kids);
+let i = 0;
+let countOfKids = kids.length;
+
+
+for( i = 0 ; i <  countOfKids ; i++){
+
+    console.log(kids[i]);
+
+}
---
+++ lecturedemos/2-6.js
@@ -0,0 +1,12 @@
+
+
+
+let i;
+for( i = 0 ; i < 5 ; i++ ){
+
+    console.log("Tick! ")
+    console.log ("i is currently the value of " + i)
+
+}
+
+console.log(i);
